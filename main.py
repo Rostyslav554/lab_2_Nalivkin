1713d6919aca1d7fe053b55b7e35b84b1f92cb51
Rostyslav554 <rstslvnlvkin@gmail.com>
2024-11-03 22:58:42 +0200

    lab_2_Nalivkin

---
+++ main.py
@@ -0,0 +1,24 @@
+print("Завдання 1:")
+print("Programming***"+"Essentials***"+"in..."+"Python\n")
+
+print("Завдання 2:")
+print('     *'+'\n   *  *'+'\n  *    *'+'\n *      *'+'\n***    ***'+ 2*'\n   *   *'+'\n   *****\n')
+
+print("Завдання 3:")
+print("I'm student\n")
+
+print("Завдання 4:")
+print("\"I'm\""+'\n""learning""'+'\n"""Python"""\n')
+
+print("Завдання 5:")
+a="500"
+b=int(a, 8)
+print(b)
+
+print("Завдання 6:")
+c="777"
+d=int(c, 16)
+print(d)
+
+a1=str(3 ** 3 ** 4)
+print('\nКонтрольне запитання №2: '+ a1)
